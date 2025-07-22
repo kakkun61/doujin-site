183941c2cd2f03b50af938fb295680c3cc384919
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-07-22 19:56:03 +0900

    index で本のタイトルがイベントのタイトルに変わってしまっていた

--- content/index.hs
+++ content/index.hs
@@ -18,10 +18,10 @@ render (path, books) =
     $ div_ [class_ "home"] $ do
         ul_ [class_ "book-list"] $ do
           F.for_ books $ \(book@Book { title, image }, path) -> do
-            let Just (Event { id, title } : _) = M.lookup book Publication.events
             li_ $ do
               h3_ $ a_ [class_ "post-link", href_ path] $ toHtml title
               div_ [class_ "justify-bottom"] $ do
+                let Just (Event { id, title } : _) = M.lookup book Publication.events
                 p_ $ span_ [class_ "event-badge"] $ a_ [href_ $ "/" <> id <> ".html"] $ toHtml title
                 a_ [href_ path] $ img_ [src_ image, alt_ "book image", class_ "home-book-front"]
   where
