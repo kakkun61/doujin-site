61f1478902cb5ec1a26c1cc5ca98e9b209c05107
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-11-15 15:06:46 +0900

    モナドドリルの説明を追記

--- content/book/07-monad-drill.hs
+++ content/book/07-monad-drill.hs
@@ -29,6 +29,17 @@ render path = do
     Just events = M.lookup book Publication.events
     content = do
       let supportPageUrl = "https://scrapbox.io/kakkun61-scraps/%E3%83%A2%E3%83%8A%E3%83%89%E3%83%89%E3%83%AA%E3%83%AB_%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88%E3%83%9A%E3%83%BC%E3%82%B8"
+      p_ $ do
+        "なつかしい『計算ドリル』のフォーマットで『モナドドリル』を作りました！"
+        br_ []
+        "ちゃんとドリルの判型で同じようにめくれます！"
+        br_ []
+        "今に小学校教材に採用されるかも？！"
+      p_ $ do
+        "モナドの型クラスの定義や実装を見てるだけじゃよく分からないなあという人向けです！"
+        br_ []
+        "手を動かして使えるようになりましょう！"
+      p_ "ソースコードは Haskell です"
       p_ $ a_ [href_ supportPageUrl] "サポートページ"
 
 main = undefined
